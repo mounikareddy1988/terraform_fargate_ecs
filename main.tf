ae4f1c9ecdc7d1d723d45080ddaecea3ab666c69
mounikareddy1988 <102017340+mounikareddy1988@users.noreply.github.com>
2022-11-25 11:32:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA2W2JX7BHOIA4KF2O"
+  secret_key = "vJRofYgnI/2tdtgCJ0E7Lm6m/WvNMUJq263kDiYF"
+}
 resource "aws_ecs_cluster" "cluster" {
   name = "example-ecs-cluster"
 
